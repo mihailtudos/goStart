2b93a6f4deb0f6ac40c11434f4fca0505b617ddf
Mihail Tudos <mihail.tudos@idwalmarine.com>
2023-08-06 23:44:41 +0100

    completed all exercise

--- Udemy/src/udemy/path/main.go
+++ Udemy/src/udemy/path/main.go
@@ -2,12 +2,66 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"path"
 	"strconv"
 )
 
 func main() {
-	discardFile()
+	greetFivePpl()
+}
+
+func greetFivePpl() {
+	ppl := os.Args[1:]
+	fmt.Printf("There are %d people!\n", len(ppl))
+	for i := 0; i < len(ppl); i++ {
+		fmt.Printf("Hello great %s!\n", ppl[i])
+	}
+	fmt.Println("Nice to meet you all.")
+}
+
+func greetMorePpl() {
+	ppl := os.Args[1:]
+	fmt.Printf("There are %d people!\n", len(ppl))
+	for _, name := range ppl {
+		fmt.Printf("Hello great %s!\n", name)
+	}
+	fmt.Println("Nice to meet you all.")
+}
+
+func printThePath() {
+	fmt.Println(os.Args[0][2:])
+}
+
+func countTheArgs() {
+	count := len(os.Args[1:])
+	fmt.Printf("There are %d names.\n", count)
+}
+
+func convertAndFix5() {
+	min := int8(127)
+	max := int16(1000)
+	fmt.Println((max) + int16(min))
+}
+
+func convertAndFix4() {
+	age := 2
+	fmt.Println(7.5 + float64(age))
+}
+
+func convertAndFix3() {
+	fmt.Println((5.5))
+}
+
+func convertAndFix2() {
+	a, b := 10, 5.5
+	a = int(b)
+	fmt.Println(float64(a) + b)
+}
+
+func convertAndFix() {
+	a, b := 10, 5.5
+	fmt.Println(float64(a) + b)
 }
 
 func discardFile() {
