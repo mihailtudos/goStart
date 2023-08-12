5da925678bfc934143a25a6f13aa7a036db2ccff
Mihail Tudos <mihail.tudos@outlook.com>
2023-08-12 15:09:04 +0100

    introduces envs and break, continue lable statements

--- Udemy/src/udemy/search/go.mod
+++ Udemy/src/udemy/search/go.mod
@@ -1,3 +1,7 @@
 module github.com/mihailtudos/search
 
 go 1.20
+
+require golang.org/x/exp v0.0.0-20230811145659-89c5cff77bcb
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- Udemy/src/udemy/search/main.go
+++ Udemy/src/udemy/search/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"golang.org/x/exp/slices"
+	"os"
+	"strings"
+)
+
 func main() {
+	if err := godotenv.Load(); err != nil {
+		panic("Something went wrong when loading the env file. STOPPING...")
+	}
+	words := strings.Fields(os.Getenv("CORPUS"))
+	filters := []string{"the", "and", "or"}
+	query := os.Args[1:]
+query:
+	for _, q := range query {
+		q = strings.ToLower(q)
+	filter:
+		for i, w := range words {
+			w = strings.ToLower(w)
+
+			if slices.Contains(filters, w) {
+				break filter
+			}
+
+			if w == q {
+				fmt.Printf("#%-2d: %q\n", i+1, w)
+				continue query
+			}
+		}
+	}
 
 }
