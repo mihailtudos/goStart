c12d16d00e78cff1188b815f0fffc681711d4a2d
Mihail Tudos <mihail.tudos@idwalmarine.com>
2023-08-20 09:12:53 +0100

    fixed issues

--- Udemy/src/udemy/slicesChapter/main.go
+++ Udemy/src/udemy/slicesChapter/main.go
@@ -7,11 +7,41 @@ import (
 	"os"
 	"sort"
 	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
-	updateToSlices()
+	fixSlicesIssue()
+}
+
+func fixSlicesIssue() {
+	var (
+		names []string
+	)
+
+	names = []string{
+		"Einstein",
+		"Shepard",
+		"Tesla",
+	}
+
+	var books []string = []string{
+		"Stay Golden",
+		"Fire",
+		"Kafka's Revenge",
+	}
+
+	sort.Strings(books)
+
+	nums := [...]int{5, 1, 7, 3, 8, 2, 6, 9}
+
+	sort.Ints(nums[:])
+
+	fmt.Printf("%s\n", strings.Join(names, " and "))
+
+	fmt.Printf("%q\n", books)
+	fmt.Printf("%d\n", nums)
 }
 
 func updateToSlices() {
