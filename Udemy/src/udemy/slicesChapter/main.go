f7d7e18ed788a4bd285c7c9313801ab6f70766ee
Mihail Tudos <mihail.tudos@idwalmarine.com>
2023-08-20 09:05:16 +0100

    literals initialized slices

--- Udemy/src/udemy/slicesChapter/main.go
+++ Udemy/src/udemy/slicesChapter/main.go
@@ -11,10 +11,24 @@ import (
 )
 
 func main() {
-	zeroSilces()
+	sliceLiterals()
 }
 
-func zeroSilces() {
+func sliceLiterals() {
+	names := []string{"Petru", "Marat", "Gabi"}
+	distances := []int{10, 20, 22, 30, 55}
+	data := []uint8{'b', 's', '2', ',', '.'}
+	ratios := []float64{1.23, 0.8}
+	alive := []bool{true, true, false, true}
+
+	fmt.Printf("names:     %T %d %t\n", names, len(names), nil == names)
+	fmt.Printf("distances: %T %d %t\n", distances, len(distances), nil == distances)
+	fmt.Printf("data:      %T %d %t\n", data, len(data), nil == data)
+	fmt.Printf("ratios:    %T %d %t\n", ratios, len(ratios), nil == ratios)
+	fmt.Printf("alive:     %T %d %t\n", alive, len(alive), nil == alive)
+}
+
+func zeroSlices() {
 	var (
 		names     []string
 		distances []int
