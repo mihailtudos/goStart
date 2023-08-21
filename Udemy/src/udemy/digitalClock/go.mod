2fcb2c04cabf748e9a27eb4ca870d959b0912b4f
Mihail Tudos <mihail.tudos@outlook.com>
2023-08-21 22:28:59 +0100

    completed digital clock

--- Udemy/src/udemy/digitalClock/go.mod
+++ Udemy/src/udemy/digitalClock/go.mod
@@ -1,3 +1,10 @@
 module github.com/mihailtudos/digitalclock
 
 go 1.20
+
+require (
+	github.com/inancgumus/screen v0.0.0-20190314163918-06e984b86ed3 // indirect
+	golang.org/x/crypto v0.12.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/term v0.11.0 // indirect
+)
--- Udemy/src/udemy/digitalClock/main.go
+++ Udemy/src/udemy/digitalClock/main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"github.com/inancgumus/screen"
 	"time"
 )
 
 type digit [5]string
 
 func main() {
-	t := time.Now()
-	h, m, s := t.Hour(), t.Minute(), t.Second()
-	clock := [8]digit{getDigit(h / 10), getDigit(h % 10), getSeparator(), getDigit(m / 10), getDigit(m % 10), getSeparator(), getDigit(s / 10), getDigit(s % 10)}
-	for range clock {
+	empty := false
+	for {
+		screen.Clear()
+		screen.MoveTopLeft()
+		t := time.Now()
+		h, m, s := t.Hour(), t.Minute(), t.Second()
+		clock := [8]digit{getDigit(h / 10), getDigit(h % 10), getSeparator(empty), getDigit(m / 10), getDigit(m % 10), getSeparator(empty), getDigit(s / 10), getDigit(s % 10)}
 
+		for i, _ := range clock[0] {
+			for _, c := range clock {
+				fmt.Printf("%-5s", c[i])
+			}
+			fmt.Println()
+		}
+		empty = !empty
+		fmt.Println()
+		time.Sleep(time.Duration(time.Millisecond * 1000))
 	}
 }
 
-func getSeparator() digit {
+func getSeparator(empty bool) digit {
+	if empty {
+		return digit{
+			"   ",
+			"   ",
+			"   ",
+			"   ",
+			"   ",
+		}
+	}
 	return digit{
 		"   ",
 		" █ ",
