91e973baf9b6d54010d0ac0e4b8b4586f776ffb0
Mihail Tudos <mihail.tudos@idwalmarine.com>
2023-09-09 13:18:47 +0100

    fixed lyrics

--- Udemy/src/udemy/slicesChapter/main.go
+++ Udemy/src/udemy/slicesChapter/main.go
@@ -18,7 +18,18 @@ import (
 const size = 1e7
 
 func main() {
-	sortBackingArray()
+	correctLyrics()
+}
+
+func correctLyrics() {
+	// DON'T TOUCH THIS:
+	lyric := strings.Fields(`all my troubles seemed so far away oh I believe in yesterday now it looks as though they are here to stay`)
+
+	// ADD YOUR CODE BELOW:
+	lyric = append([]string{"yesterday"}, lyric...)
+	lyric = append(lyric, lyric[8:13]...)
+	lyric = append(lyric[:8], lyric[13:]...)
+	fmt.Printf("%s\n", lyric)
 }
 
 func sortBackingArray() {
